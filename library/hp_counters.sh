46bf07b8e6024d43c8e82ea81889b40136f69fd4
max <m.mattioli67@gmail.com>
2022-11-21 16:44:27 +0100

    go home

---
+++ library/hp_counters.sh
@@ -0,0 +1,199 @@
+#!/bin/bash
+COUNT=0
+oid=7
+MOD=0
+val=0
+initial="zero"
+ip=$1
+export ip
+sp=" "
+trat="-"
+echo $ip;
+#file2=$(cat  /home/max/ansible/procurve/int_up |tr "\n" " ")
+path="/home/max/ansible/module/procurve"
+snmpwalk -v 2c -c pubrim $ip .1.3.6.1.2.1.2.2.1.2\
+ | sed -n -e 's/^.*STRING: //p' &>> "$path"/int; 
+file1=$(cat  /home/max/ansible/module/procurve/int |tr "\n" " ")
+snmpwalk -v 2c -c pubrim $ip .1.3.6.1.2.1.2.2.1.8\
+ | sed -n -e 's/^.*INTEGER: //p' &>> "$path"/int_up; 
+file2=$(cat  /home/max/ansible/module/procurve/int_up |tr "\n" " ")
+snmpbulkwalk -v 2c -c pubrim $ip 1.0.8802.1.1.2.1.4.1.1.9\
+| sed -n -e 's/^.*\.\(.*\)\.\(.* =.*\).*\(SW\|SWT\|sw\|swt\).*$/\1/p' &>> "$path"/rem_id;
+file3=$(cat  /home/max/ansible/module/procurve/rem_id |tr "\n" " ")
+hostname=$(snmpget -v 2c -c pubrim $ip sysName.0 | sed -n -e 's/^.*STRING: //p')
+lldpItem=($file3)
+Lengthid=${#lldpItem[@]}
+file5=$path/rem_id;
+while IFS= read -r line; do
+  intid=$(echo "$line");
+  oid=".1.3.6.1.2.1.2.2.1.2."$intid
+  snmpget -v 2c -c pubrim $ip $oid\
+  | sed -n -e 's/^.*STRING: //p' &>> "$path"/rem_int;
+done < "$file5"
+snmpwalk -v 2c -c pubrim $ip 1.3.6.1.2.1.31.1.1.1.6\
+ |  sed -n -e 's/\(^.*\.\)\(.*\)\( = \)\(Counter64: [1-9].*$\)/\2/p' &>> "$path"/counters;
+file6=$path/counters;
+while IFS= read -r line; do
+  intid=$(echo "$line");
+  oid=".1.3.6.1.2.1.2.2.1.2."$intid
+  snmpget -v 2c -c pubrim $ip $oid\
+  | sed -e 's/^\(.\{12\}\).*//' | sed -e '/^\s*$/d' &>> "$path"/count_int;
+done < "$file6"
+file7=$(cat  /home/max/ansible/module/procurve/count_int |tr "\n" " ")
+remcount=($file7)
+#'''for (( j = 0; j < Lengthid; ++ j ));
+#do
+#  oid=".1.3.6.1.2.1.2.2.1.2.${lldpItem[$j]:0:1}"
+#  snmpget -v 2c -c pubrim $ip $oid\
+#   | sed -n -e 's/^.*STRING: //p' &>> "$PATH"/rem_int;
+#done'''
+file4=$(cat  /home/max/ansible/module/procurve/rem_int |tr "\n" " ")
+remItem=($file4)
+#only delete items if there lldp interfaces:
+##for del in ${file4[@]}
+##do
+##   file1=("${file1[@]/$del}") #Quotes when working with strings
+##done
+Lengthrem=${#remItem[@]}
+fileItem=($file1)
+echo $file1
+intupItem=($file2)
+Length=${#fileItem[@]}
+echo "$Length";
+echo $file1
+rm -f $path/rem*;
+rm -f $path/int*;
+rm -f $path/lldp_i*;
+rm -f $path/Eth_int;
+rm -f $path/camp*;
+rm -f $path/add_camp*;
+rm -f $path/count*;
+rm -f $path/add_camp*;
+#
+#the following it's been commented (see the script master)
+##verpath=/home/max/ansible/module/host_vars/$hostname.yml;
+##if [[ -e "$verpath" ]];then
+##	msg="the file in host_vars folder must not be present yet. Exiting"
+##	exit 0
+##else
+##  #
+##  touch $verpath;
+##  echo "---" &>>$verpath;
+##  echo "loop_protect:" &>>$verpath;
+##fi
+###
+for (( i = 0; i < Length; ++ i ));
+do
+  if [[ ${fileItem[$i]:0:1} =~ [a-zA-Z] && ${fileItem[$i]:1:1} =~ [0-9] ]]; then
+    if [[ $initial == zero ]]; then
+      initial=$fileItem;
+      echo "$initial" &>>$path/camp$COUNT;
+      #last=${fileItem[$i+1]};
+      #MOD=$(expr $MOD + 1);
+    else
+      #echo "$fileItem" | sed -e 's/\(^\)\(.*\)/  - \1\2/g' &>>$verpath;
+      if [[ ${fileItem[$i]:0:1} == ${fileItem[$i+1]:0:1} && ${fileItem[$i+1]:1:1} =~ [0-9] ]]; then
+        if [[ $MOD == 0 ]]; then
+          initial=${fileItem[$i]};
+          echo "$initial" &>>$path/camp$COUNT;
+          MOD=$(expr $MOD + 1);
+        fi
+        last=${fileItem[$i+1]};
+        echo "$last" &>>$path/camp$COUNT;
+      else
+        COUNT=$(expr $COUNT + 1);
+        #initial=${fileItem[$i+1]};
+        integer=$(expr $i + 1);
+        #if ![[ $integer == Length ]]; then
+        #  echo "$initial" &>>$path/camp$COUNT;
+          echo "$COUNT" &> $path/Eth_int;
+          MOD=0;
+        #fi
+        #echo "$initial" &>>$path/camp$COUNT;
+        #echo "$COUNT" &> $path/Eth_int;
+        #MOD=0;
+      fi 
+    fi
+  #fi
+  elif [[ ${fileItem[$i]:0:1} =~ [1-9] && ${fileItem[$i]:1:1} =~ '/' &&  ${fileItem[$i]:2:1} =~ [1-9] ]]; then
+    if [[ $initial == zero ]]; then
+      initial=$fileItem;
+      echo "$initial" &>>$path/camp$COUNT;
+      #last=${fileItem[$i+1]};
+      MOD=$(expr $MOD + 1);
+    else
+      if [[ ${fileItem[$i]:0:1} == ${fileItem[$i+1]:0:1} && ${fileItem[$i+1]:2:1} =~ [0-9] ]]; then
+        last=${fileItem[$i+1]};
+        echo "$last" &>>$path/camp$COUNT;
+        #echo "$fileItem" &>>$path/camp$COUNT;
+      else
+        COUNT=$(expr $COUNT + 1);
+        initial=${fileItem[$i+1]};
+        echo "$initial" &>>$path/camp$COUNT;
+        echo "$COUNT" &> $path/Eth_int;
+        MOD=0;
+      fi
+    fi
+     #MOD=0;
+  elif [[ ${fileItem[$i]:0:1} =~ [1-9] && ${fileItem[$i]:1:1} =~ '/' &&  ${fileItem[$i]:2:1} =~ [a-zA-Z] ]]; then
+    if [[ $initial == zero ]]; then
+      initial=$fileItem;
+      echo "$initial" &>>$path/camp$COUNT;
+      #last=${fileItem[$i+1]};
+      MOD=$(expr $MOD + 1);
+    else
+      if [[ ${fileItem[$i]:0:1} == ${fileItem[$i+1]:0:1} && ${fileItem[$i+1]:2:1} =~ [a-zA-Z] ]]; then
+        last=${fileItem[$i+1]};
+        echo "$last" &>>$path/camp$COUNT;
+        #echo "$fileItem" &>>$path/camp$COUNT;
+      else
+        COUNT=$(expr $COUNT + 1);
+        initial=${fileItem[$i+1]};
+        echo "$initial" &>>$path/camp$COUNT;
+        echo "$COUNT" &> $path/Eth_int;
+        MOD=0;
+      fi
+    fi
+  #fi
+  elif [[ ${fileItem[$i]:0:1} =~ [0-9]  ]]; then
+    #second if condition set initial to first line character (if initial==zero)
+    if [[ $initial == zero ]]; then
+      initial=$fileItem;
+      echo "$initial" &>>$path/camp$COUNT;
+      #last=${fileItem[$i+1]};
+      MOD=$(expr $MOD + 1);
+    else
+      if [[ ${fileItem[$i]:0:1} =~ [0-9] && ${fileItem[$i+1]:0:1} =~ [0-9] ]]; then
+        last=${fileItem[$i+1]};
+        echo "$last" &>>$path/camp$COUNT;
+      else
+        COUNT=$(expr $COUNT + 1);
+        initial=${fileItem[$i+1]};
+        integer=$(expr $i + 1);
+        #if ![[ $integer == Length ]]; then
+          echo "$initial" &>>$path/camp$COUNT;
+          echo "$COUNT" &> $path/Eth_int;
+          MOD=0;
+        #fi
+      fi
+    fi
+  elif [[ ${fileItem[$i]:0:1} =~ [tT] && ${fileItem[$i]:1:1} =~ [rR] ]]; then
+    if [[ $initial == zero ]]; then
+      #echo "$initial" &>>$path/camp$COUNT;
+      #last=${fileItem[$i+1]};
+      MOD=$(expr $MOD + 1);
+    else
+      #statements
+    #now is time to build camp var with elements
+      initial=${fileItem[$i]}
+      if [[ $initial == $last ]]; then
+        :
+      #COUNT=$(expr $COUNT + 1);
+      echo "$initial" &>>$path/camp$COUNT;
+      #COUNT=$(expr $COUNT + 1);
+      #initial=${fileItem[$i+1]};
+      echo "$COUNT" &> $path/Eth_int;
+      #next condition try to verify if next line character is a number if so set last var
+     fi
+  fi
+done
